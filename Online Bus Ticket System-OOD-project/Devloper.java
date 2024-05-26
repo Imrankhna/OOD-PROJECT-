aecd4c6cdb0e5a2981b6b23f1afa9f71e776a9f7
Imrankhna <153683422+Imrankhna@users.noreply.github.com>
2024-05-27 00:48:50 +0600

    Add files via upload

---
+++ Online Bus Ticket System-OOD-project/Devloper.java
@@ -0,0 +1,14 @@
+public class Devloper {
+    void dev()
+    {
+        System.out.println("MD.Imran Khan");
+        System.out.println("0242220005341049");
+        System.out.println("B.Sc in SWE At DIU");
+    }
+
+    void help()
+    {
+        System.out.println("help.public@gmail.com");
+        System.out.println("Number_ 888");
+    }
+}
